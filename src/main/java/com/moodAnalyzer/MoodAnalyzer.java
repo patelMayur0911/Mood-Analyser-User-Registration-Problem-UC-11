c5d2d1dc2b7bcc0692705e869b34e3dc1f705551
Patel_Mayur <patelmayuraaa@gmail.com>
2024-01-01 18:37:48 +0530

    added main fn

--- src/main/java/com/moodAnalyzer/MoodAnalyzer.java
+++ src/main/java/com/moodAnalyzer/MoodAnalyzer.java
@@ -8,4 +8,14 @@ public class MoodAnalyzer {
             return "Happy";
         }
     }
+
+    public static void main(String[] args) {
+        MoodAnalyzer moodAnalyzer = new MoodAnalyzer();
+
+        String message1 = "I am feeling Sad today.";
+        System.out.println("Mood 1: " + message1);
+
+        String message2 = "I am feeling Happy!";
+        System.out.println("Mood 2: " + message2);
+    }
 }
